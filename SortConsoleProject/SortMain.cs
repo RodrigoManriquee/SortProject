2cdb797a7b51983c9e8f31d487f132f5fc2a98ad
Rodrigo <rodrigomanriquep@gmail.com>
2024-04-01 22:10:53 -0700

    First Commit, sort int and array

---
+++ SortConsoleProject/SortMain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortConsoleProject
+{
+    class SortMain
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 4, 2, 7, 1, 9, 5 };
+            string[] stringArray = { "banana", "apple", "orange", "grape", "kiwi" };
+
+            Console.WriteLine("Sorted integer array:");
+            foreach (var item in intArray.OrderBy(x => x))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\nSorted string array:");
+            foreach (var item in stringArray.OrderBy(x => x))
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
