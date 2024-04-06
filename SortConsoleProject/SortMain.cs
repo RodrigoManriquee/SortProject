d6988c4ac4fc182b226e3f3ea5dfe390398e3f4b
Rodrigo <rodrigomanriquep@gmail.com>
2024-04-05 19:48:48 -0700

    .

--- SortConsoleProject/SortMain.cs
+++ SortConsoleProject/SortMain.cs
@@ -12,7 +12,6 @@ namespace SortConsoleProject
         {
             int[] intArray = { 4, 2, 7, 1, 9, 5 };
 
-            //test
             Console.WriteLine("Original integer array:");
             PrintIntArray(intArray);
             SortableArray.SortIntArray(intArray);
